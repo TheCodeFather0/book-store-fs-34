268a86f39f78b47e2271a022a4ae18250ad4634a
TheCodeFather0 <raminmammadov056@gmail.com>
2025-04-18 20:22:55 +0400

    getAllBooks completed

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import homeController from "./controllers/home.js";
+import getAllBooks from "./controllers/getAllBooks.js";
 
 const port = 8080;
 const app = express();
 
 app.get("/", homeController);
+app.get("/books", getAllBooks);
 
 app.listen(port, () => {
   console.log(`server http://localhost:${port} ünvanında işləyir!`);
