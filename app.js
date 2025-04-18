6e48662976787e9597166283e53a1d91b50288e4
TheCodeFather0 <raminmammadov056@gmail.com>
2025-04-18 20:59:00 +0400

    createNewBook controller added

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import homeController from "./controllers/home.js";
 import getAllBooks from "./controllers/getAllBooks.js";
+import createNewBook from "./controllers/createNewBook.js";
 
 const port = 8080;
 const app = express();
+app.use(express.json());
 
 app.get("/", homeController);
 app.get("/books", getAllBooks);
+app.post("/books", createNewBook);
 
 app.listen(port, () => {
   console.log(`server http://localhost:${port} ünvanında işləyir!`);
