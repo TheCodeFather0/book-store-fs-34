1c3947e5cb11aabedd89e36d2fa3a9dcda93452e
TheCodeFather0 <raminmammadov056@gmail.com>
2025-04-21 19:54:01 +0400

    get and delete by id added

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 import express from "express";
 import homeController from "./controllers/home.js";
 import getAllBooks from "./controllers/getAllBooks.js";
+import getBookById from "./controllers/getBookById.js";
 import createNewBook from "./controllers/createNewBook.js";
+import deleteBookById from "./controllers/deleteBookById.js";
 
 const port = 8080;
 const app = express();
 app.use(express.json());
 
+// GET REQUESTS
 app.get("/", homeController);
 app.get("/books", getAllBooks);
+app.get("/books/:id", getBookById);
+
+// POST REQUESTS
 app.post("/books", createNewBook);
+
+// DELETE REQUESTS
+app.delete("/books/:id", deleteBookById);
 
 app.listen(port, () => {
   console.log(`server http://localhost:${port} ünvanında işləyir!`);
